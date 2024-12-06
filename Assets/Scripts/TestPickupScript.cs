91357dceba0211f3b002e85bddd2369c13165e74
Tenzin2234 <tlungrik@macalester.edu>
2024-12-06 11:51:32 -0600

    update

---
+++ Assets/Scripts/TestPickupScript.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPickupScript : MonoBehaviour
+{
+ public gameObject player;
+ public Transform holpos;
+  
+}
